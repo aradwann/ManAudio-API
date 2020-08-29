fa482f38f94f79c03512cf9917befcddf009611f
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-08-29 19:44:00 +0200

    add registeration view

---
+++ app/auth/views.py
@@ -0,0 +1,42 @@
+from . import auth
+from app import db
+from flask import request, make_response, jsonify
+from app.models import User
+
+
+@auth.route('/register', methods=['POST'])
+def register():
+    # get the post data
+    post_data = request.get_json()
+    # check if user already exists
+    user = User.query.filter_by(email=post_data.get('email')).first()
+    if not user:
+        try:
+            user = User(
+                email=post_data.get('email'),
+                password=post_data.get('password')
+            )
+
+            # insert the user
+            db.session.add(user)
+            db.session.commit()
+            # generate the auth token
+            auth_token = user.encode_auth_token(user.id)
+            responseObject = {
+                'status': 'success',
+                'message': 'Successfully registered.',
+                'auth_token': auth_token.decode()
+            }
+            return make_response(jsonify(responseObject)), 201
+        except Exception as e:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Some error occurred. Please try again.'
+            }
+            return make_response(jsonify(responseObject)), 401
+    else:
+        responseObject = {
+            'status': 'fail',
+            'message': 'User already exists. Please Log in.',
+        }
+        return make_response(jsonify(responseObject)), 202
