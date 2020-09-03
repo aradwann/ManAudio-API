e7daa0a79834c540b87d923372aa875569f86934
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-09-03 14:14:43 +0200

    test user login view

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -45,3 +45,66 @@ def test_registered_with_already_registered_user(client):
         data['message'] == 'User already exists. Please Log in.')
     assert (response.content_type == 'application/json')
     assert (response.status_code == 202)
+
+
+def test_registered_user_login_correct_password(client):
+    """Test registered user login"""
+    user = User(
+        email='joe@gmail.com',
+        password='test'
+    )
+    db.session.add(user)
+    db.session.commit()
+    response = client.post(
+        '/auth/login',
+        data=json.dumps(dict(
+            email='joe@gmail.com',
+            password='test'
+        )),
+        content_type='application/json')
+    data = json.loads(response.data.decode())
+    assert data['status'] == 'success'
+    assert data['message'] == 'Successfully logged in.'
+    assert data['auth_token']
+    assert response.content_type == 'application/json'
+    assert response.status_code == 200
+
+
+def test_registered_user_login_wrong_password(client):
+    """Test registered user login"""
+    user = User(
+        email='joe@gmail.com',
+        password='test'
+    )
+    db.session.add(user)
+    db.session.commit()
+    response = client.post(
+        '/auth/login',
+        data=json.dumps(dict(
+            email='joe@gmail.com',
+            password='testo'
+        )),
+        content_type='application/json')
+    data = json.loads(response.data.decode())
+    assert data['status'] == 'fail'
+    assert data['message'] == 'Wrong password.'
+    assert not data.get('auth_token')
+    assert response.content_type == 'application/json'
+    assert response.status_code == 401
+
+
+def test_non_registered_user_login(client):
+    """Test non registered user login"""
+    response = client.post(
+        '/auth/login',
+        data=json.dumps(dict(
+            email='joe@gmail.com',
+            password='test'
+        )),
+        content_type='application/json')
+    data = json.loads(response.data.decode())
+    assert data['status'] == 'fail'
+    assert data['message'] == 'User does not exist.'
+    assert not data.get('auth_token')
+    assert response.content_type == 'application/json'
+    assert response.status_code == 401
