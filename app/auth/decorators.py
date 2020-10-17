5b4702c8794f9deaaa444d3f7e23dd51521057c1
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-10-17 18:40:32 +0200

    implment auth_required decorator

--- app/auth/decorators.py
+++ app/auth/decorators.py
@@ -1,5 +1,7 @@
 from flask import request
 from .errors import AuthError
+from ..models import User
+from functools import wraps
 
 
 def get_token_auth_header():
@@ -9,26 +11,41 @@ def get_token_auth_header():
     """
     auth_header = request.headers.get('Authorization', None)
     if not auth_header:
-        raise AuthError({
-            'status': 'fail',
-            'message': 'Authorization header is expected.'
-        }, 401)
+        raise AuthError(
+            status_code=401,
+            message='Authorization header is expected.'
+        )
 
     auth_parts = auth_header.split()
     if auth_parts[0].lower() != 'bearer':
-        raise AuthError({
-            'status': 'fail',
-            'message': 'Authorization header must start with "Bearer".'
-        }, 401)
+        raise AuthError(
+            status_code=401,
+            message='Authorization header must start with "Bearer".'
+        )
     elif len(auth_parts) == 1:
-        raise AuthError({
-            'status': 'fail',
-            'message': 'Token not found.'
-        }, 401)
+        raise AuthError(
+            status_code=401,
+            message='Token not found.'
+        )
     elif len(auth_parts) > 2:
-        raise AuthError({
-            'status': 'fail',
-            'message': 'Token not found.'
-        }, 401)
+        raise AuthError(
+            status_code=401,
+            message='Token not found.'
+        )
     token = auth_parts[1]
     return token
+
+
+def auth_required(func):
+    """require the request sender to be authenticated"""
+    @wraps(func)
+    def wrapper_auth_required(*args, **kwargs):
+        token = get_token_auth_header()
+        resp = User.decode_auth_token(token)
+        if isinstance(resp, str):
+            raise AuthError(
+                status_code=401,
+                message=resp
+            )
+        return func(resp, *args, **kwargs)
+    return wrapper_auth_required
