f79b4eb81e5530d2800f045d20d23adbe3c9ee0e
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-10-21 11:36:54 +0200

    use flask-rq2

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ import config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
+from flask_rq2 import RQ
 
 # creating an app factory function to register blueprints with it and
 # accessing the app in the project through a proxy of created app called
@@ -14,19 +15,22 @@ from flask_bcrypt import Bcrypt
 db = SQLAlchemy()
 migrate = Migrate()
 bcrypt = Bcrypt()
+rq = RQ()
 
 
 def create_app(testing=False):
     """Application factory
 
     Args:
-        testing (bool, optional): will load testing config if True. Defaults to False.
-    Returns: 
+        testing (bool, optional): will load testing config if True.
+         Defaults to False.
+    Returns:
         the flask application object
     """
     app = Flask(__name__)
 
-    # Dynamically load config based on the testing argument or FLASK_ENV evironment variable
+    # Dynamically load config based on the testing argument
+    # or FLASK_ENV evironment variable
     flask_env = os.getenv('FLASK_ENV', None)
     if testing or flask_env == 'testing':
         app.config.from_object(config.TestingConfig)
@@ -39,6 +43,7 @@ def create_app(testing=False):
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
+    rq.init_app(app)
 
     # Import and register blueprints
     from app.audio import audio
