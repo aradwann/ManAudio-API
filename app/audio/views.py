0f1d204de0a3ddb460a552099ba3b55254eed8da
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-10-22 19:00:47 +0200

    refactor to use the decorated job function

--- app/audio/views.py
+++ app/audio/views.py
@@ -1,30 +1,22 @@
-import pathlib
 from . import audio
 from flask import (request,
                    jsonify,
                    make_response,
                    current_app,
-                   send_file,
                    send_from_directory,
-                   safe_join,
-                   abort,
-                   url_for,
-                   redirect,
-                   session)
+                   abort)
 from werkzeug.utils import secure_filename
 from .audio_bass_boost import export_bass_boosted
 
-from redis import Redis
-from rq import Queue, get_current_job
-
-redis_conn = Redis()
-q = Queue(connection=redis_conn)
+from app import rq
 
 ALLOWED_EXTENSIONS = set(['mp3', 'aac', 'wav', 'flac'])
 
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return ('.' in filename
+            and
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS)
 
 
 @audio.route("/", methods=['GET', 'POST'])
@@ -35,6 +27,7 @@ def index():
         req = request.get_json()
         # make response body
         response_body = {
+            "success": True,
             'message': 'JSON Recieved',
             'sender':   req.get('name')
         }
@@ -42,53 +35,69 @@ def index():
         res = make_response(jsonify(response_body), 200)
         return res
     else:
-        return make_response(jsonify({"message": "Request body must be JSON"}), 400)
+        return make_response(
+            jsonify({"success": False,
+                     "message": "Request body must be JSON"}), 400)
 
 
 @audio.route("/bass-boost", methods=['GET', 'POST'])
 def bass_boost():
     if request.method == 'POST':
+        default_queue = rq.get_queue()
+        default_queue.empty()
         if 'audio' not in request.files:
-            resp = jsonify({'message': 'No file part in the request'})
-            resp.status_code = 400
+            resp = make_response(
+                jsonify({"success": False,
+                         'message': 'No file part in the request'}), 400)
             return resp
-        file = request.files['audio']
-        if file.filename == '':
-            resp = jsonify({'message': 'No file selected for uploading'})
-            resp.status_code = 400
+        audio_file = request.files['audio']
+        if audio_file.filename == '':
+            resp = make_response(
+                jsonify({"success": False,
+                         'message': 'No file selected for uploading'}), 400)
             return resp
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+        if audio_file and allowed_file(audio_file.filename):
+            filename = secure_filename(audio_file.filename)
+            upload_dir = current_app.config['AUDIO_UPLOADS']
+            audio_file.save(upload_dir.joinpath(filename))
 
-            file.save(current_app.config["AUDIO_UPLOADS"].joinpath(filename))
-            job = q.enqueue(export_bass_boosted, args=(
-                current_app.config['AUDIO_UPLOADS'], filename), job_id='my_audio_proc')
-            # session['FILENAME'] = filename
-            # session['JOB_ID'] = 'my_audio_proc'
-            resp = jsonify({'message': 'File successfully uploaded'})
+            job = export_bass_boosted.queue(upload_dir, filename)
+
+            resp = make_response(
+                jsonify({"success": True,
+                         'message': 'File successfully uploaded'}), 201)
             resp.set_cookie('FILENAME', filename)
-            resp.status_code = 201
-            resp.set_cookie('FILENAME', filename)
-            resp.set_cookie('JOB_ID', 'my_audio_proc')
+            resp.set_cookie('JOB_ID', job.id)
             return resp
         else:
-            resp = jsonify(
-                {'message': 'Allowed file types are mp3, aac, wav, flac'})
-            resp.status_code = 400
+            resp = make_response(
+                jsonify(
+                    {"success": False,
+                     'message': 'Allowed file types are mp3, aac, wav, flac'}),
+                400)
             return resp
     if request.method == 'GET':
-        # try:
 
-        job = q.fetch_job(request.cookies.get('JOB_ID'))
-        print(request.cookies.get('FILENAME'))
-        print(job.get_status())
-        if job.get_status() != 'finished':
-            resp = jsonify(
-                {'message': 'Processing'})
-            resp.status_code = 200
-            return resp
+        print('cookie job id: ', request.cookies.get('JOB_ID'))
+        default_queue = rq.get_queue()
+        print('list of job IDs from the queue: ', default_queue.jobs)
+        job = default_queue.fetch_job(request.cookies.get('JOB_ID'))
+        print('Status: ', job.get_status())
+
+        if job.is_finished:
+            try:
+                export_dir = current_app.config["AUDIO_EXPORTS"]
+                file_name = request.cookies.get(
+                    'FILENAME').replace(".mp3", "-export.mp3")
+                return send_from_directory(export_dir,
+                                           filename=file_name,
+                                           as_attachment=True)
+
+            except FileNotFoundError:
+                abort(404)
+
         else:
-            return send_from_directory(current_app.config["AUDIO_EXPORTS"], filename=request.cookies.get('FILENAME').replace(".mp3", "-export.mp3"), as_attachment=True)
-
-        # except FileNotFoundError:
-        #         abort(404)
+            resp = make_response(jsonify(
+                {"success": True,
+                 'message': 'Processing'}), 200)
+            return resp
