9f45b041c300f24e0fbc18ed2589564eb0cec382
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-10-29 10:23:08 +0200

    update config with audio test path and JWT_EXP

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ base_dir = Path(__file__).parent.absolute()
 class Config:
     DEBUG = False
     TESTING = False
-    SECRET_KEY = os.environ.get("SECRET_KEY", 'anydfsfsafaga')
+    SECRET_KEY = os.getenv("SECRET_KEY", 'anydfsfsafaga')
 
     # SQLALCHEMY_DATABASE_URI = 'postgresql:///example'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -22,10 +22,11 @@ class Config:
     AUDIO_EXPORTS = base_dir.joinpath(
         'media/audio/exports')
     SESSION_COOKIE_SECURE = True
+    JWT_EXP = 604800
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
         'sqlite:///' + str(base_dir.joinpath('data.sqlite'))
 
 
@@ -33,7 +34,7 @@ class DevelopmentConfig(Config):
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 4
     # SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URL') or \
         'sqlite:///' + str(base_dir.joinpath('data-dev.sqlite'))
     SESSION_COOKIE_SECURE = False
 
@@ -43,9 +44,15 @@ class TestingConfig(Config):
 
     BCRYPT_LOG_ROUNDS = 4
     # SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name + '_test'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL') or \
         'sqlite://'
     PRESERVE_CONTEXT_ON_EXCEPTION = False
 
     SESSION_COOKIE_SECURE = False
     RQ_CONNECTION_CLASS = 'fakeredis.FakeStrictRedis'
+    AUDIO_UPLOADS = base_dir.joinpath(
+        'tests/audio/uploads')
+    AUDIO_EXPORTS = base_dir.joinpath(
+        'tests/audio/exports')
+    RQ_ASYNC = False
+    JWT_EXP = 5
